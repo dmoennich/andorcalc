245d43d97cd76572443c4009e43c1c2120e80e1a
Daniel Moennich <daniel.moennich@gmail.com>
2015-12-21 14:47:55 +0100

    Started TypeScript setup.

---
+++ browser/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by daniel on 21.12.15.
+ */
+var app = angular.module("AndorCalc", []);
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,11 @@ fields.main = require("../../public/data/fields/fields_main.json");
 router.use(require("morgan")("dev"));
 
 router.use(express.static(path.join(__dirname, "../../public")));
+router.use(express.static(path.join(__dirname, "../../node_modules")));
+
+router.get("/", function (req, resp) {
+	resp.sendFile(path.join(__dirname, "../../browser/index.html"));
+});
 
 router.get("/fields/:fieldsName", function (req, resp, next) {
 	var reqFields = fields[req.params.fieldsName];
